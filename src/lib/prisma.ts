7867eaf279a9285e87b106b5c8260d4fa8129069
mitul513 <mitulbaraiya513@gmail.com>
2026-05-11 10:46:25 +0530

    Refactor Prisma client initialization

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,9 +3,13 @@ import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import path from "path";
 
 
+
+
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
+
+
 
 
 function createPrismaClient() {
@@ -17,8 +21,14 @@ function createPrismaClient() {
 }
 
 
+
+
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 
+
+
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+
 
